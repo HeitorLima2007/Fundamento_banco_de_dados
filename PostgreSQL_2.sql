e52810245341c5f3b5ed11247dd2375c51dbe2ab
Heitor Lima  de Arruda <161778786+HeitorLima2007@users.noreply.github.com>
2024-08-29 21:28:55 -0400

    manipulação de dados, ligação de tabelas
    Aprendemos mais sobre manipulação de dados e começamos a aprender sobre chaves primárias e estrangeiras criando novas tabelas

--- PostgreSQL_2.sql
+++ PostgreSQL_2.sql
@@ -70,3 +70,59 @@ select 'Investimento: ' || sum(valor_inicial) ||' Rendimentos ' || sum(rendiment
 select avg(recebimento_mensal), round(avg(recebimento_mensal)) from investimentos
 
 select round(avg(recebimento_mensal),2) from investimentos
+
+--Descobri a média do rendimento mensal com ceil - inteiro mais próximo maior ou igual ao argumento arendonda para cima
+select avg(recebimento_mensal),ceil(avg(recebimento_mensal)) from investimentos
+
+--Descobri a média do rendimento mensal com floor  - inteiro mais próximo menor ou igual ao argumento arendonda para baixo
+select avg(recebimento_mensal),floor(avg(recebimento_mensal)) from investimentos
+
+--Group By 
+-- é usada para agrupar linhas que tem os mesmos valores em colunas específicas, permitindo-nos realizar operações de agregação em cada grupo
+
+--Retornar o número total de investimentos cadastrados agrupados por grupo de investimento:
+select  nome, count(*),sum(recebimento_mensal) from investimentos  group by nome
+
+-------------- Having --------------
+--permite filtrar o conjunto de resultados usando dados agregados
+
+--Retornar o número total de investimentos cadastrados agrupados por grupo de investimento:
+SELECT nome, COUNT(*), SUM(recebimento_mensal) from investimentos group by nome HAVING sum(recebimento_mensal)>=200
+
+--Retornar o número total de investimentos cadastrados agrupados por grupo de investimento e com operadores lógicos
+SELECT nome, count(*), sum(recebimento_mensal) from investimentos group by nome having sum(recebimento_mensal)>=200 and count(*)<6
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+CREATE table estoque(
+
+  id_estoque integer PRIMARY key not NULL,
+  nome VARCHAR(250),
+  cnpj VARCHAR(20) UNIQUE not NULL
+  
+);
+
+create TABLE nota_fiscal(
+
+  id_nota_fiscal INTEGER PRIMARY KEY NOT NULL,
+  qta_itens INTEGER CHECK(qta_itens > 0),
+  num_comprador INTEGER,
+  data_compra TIMESTAMP,
+  valor_total numeric(10,2)
+  
+);
+
+CREATE TABLE produtos(
+
+  id_produtos INTEGER PRIMARY KEY,
+  id_nota_fiscal INTEGER,
+  id_estoque INTEGER,
+  nome VARCHAR(100),
+  descricao VARCHAR(300),
+  avariado CHAR(1),
+  valor NUMERIC(7,2) check(valor > 0),
+  validade DATE,
+  CONSTRAINT fk_id_nota_fiscal foreign key(id_nota_fiscal) REFERENCES nota_fiscal(id_nota_fiscal),
+  constraint fk_id_estoque foreign key(id_estoque) REFERENCES estoque(id_estoque)
+  
+);
